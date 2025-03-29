746847c2051f6f98c921328486269b61451baef0
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-03-29 22:47:42 +0500

    add favicon

--- src/components/ui/logo/Logo.tsx
+++ src/components/ui/logo/Logo.tsx
@@ -4,9 +4,12 @@ import Link from 'next/link'
 
 export function Logo() {
 	return (
-		<Link href={PUBLIC_URL.home()} className='text-main flex gap-1.5 items-center justify-center text-2xl hover:opacity-75 transition-opacity self-start'>
-			<Image src='/images/logo.png' alt='logo' width={80} height={80} />
-			MindCare
+		<Link
+			href={PUBLIC_URL.home()}
+			className='text-main flex gap-1.5 items-center justify-center text-4xl font-semibold hover:opacity-75 transition-opacity self-start'
+		>
+			<Image src='/icons/Logo.png' alt='logo' width={55} height={55} />
+			Mind<span className='text-second'>Care</span>
 		</Link>
 	)
 }
