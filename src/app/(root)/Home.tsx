20467b3269111f6e251840c0789bd12e8f84f97d
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-03-30 22:36:00 +0500

    Help section

--- src/app/(root)/Home.tsx
+++ src/app/(root)/Home.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { FormSection } from "./Sections/form-section/FormSection"
-import { Helps } from "./Sections/helps/Helps"
+import { Helps } from "./Sections/Helps/Helps"
 import HeroSection from "./Sections/Hero"
 import { NewsSection } from "./Sections/news/NewsSection"
 import { Qansw } from "./Sections/qansw/Qansw"
