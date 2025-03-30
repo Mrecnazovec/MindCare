ae09403d577fd24dc66a7cb55d0f81b701aff0ad
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-03-30 22:39:44 +0500

    hope fix

--- src/app/(root)/Home.tsx
+++ src/app/(root)/Home.tsx
@@ -3,9 +3,9 @@
 import { FormSection } from "./Sections/form-section/FormSection"
 import { Helps } from "./Sections/Helps/Helps"
 import HeroSection from "./Sections/Hero"
-import { NewsSection } from "./Sections/news/NewsSection"
-import { Qansw } from "./Sections/qansw/Qansw"
-import { Specialists } from "./Sections/specialists/Specialists"
+import { NewsSection } from "./Sections/News/NewsSection"
+import { Qansw } from "./Sections/Qansw/Qansw"
+import { Specialists } from "./Sections/Specialists/Specialists"
 
 
 
